ca448c8bbf0e7fb0707ee2793f28fbf071e30f26
NatashaAid <natasha.aidomon@gmail.com>
2024-09-05 12:00:34 +0100

    Initial frontend setup for Harry Potter web app

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const searchInput=document.getElementById('search')
 
 let characterArray=[]
 
+searchInput.addEventListener("input", (e)=>{
+    const value=e.target.value.toLowerCase();
+    console(value)
+    characterArray.forEach((character)=>{
+        const isVisible=character.name.toLowerCase().
+        includes(value);
+        character.element.classList.toggle("hide",!isVisible);
+    })
+})
+
+
 getCharacters()
 
 function getCharacters(){
-    fetch('https://potterhead-api.vercel.app/api/characters')
+    fetch('https://harry-potter-api-day-2.onrender.com/characters')
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
@@ -17,6 +28,40 @@ function getCharacters(){
 
 
 function showCharacters(data){
-    main.innerHTML=""
+    main.innerHTML='';
+
+    characterArray=data.map(character=>{
+        const {name, image}=character;
+
+        const characterEl=document.createElement('div');
+        characterEl.classList.add('character');
+        characterEl.classList.add('front');
+
+        characterEl.innerHTML= `<img src=${image} alt=${name} oneerror="handleError(this);"/>
+        <div class="character-info">
+        <h3>${name}</h3>
+        </div>`
+        
+        const characterBack=document.createElement('div');
+        characterBack.classList.add('character');
+        characterBack.classList.add('back');
+
+        characterBack.innerHTML=`<h1>Back of the card</h1>`
+
+        main.appendChild(characterEl);
+        main.appendChild(characterBack);
+        return {
+            name:character.name,
+            element:characterEl
+        }
+
+    })
+
+
+
 }
 
+function handleError(imgElement) {
+    imgElement.src="images/wizard-icon.jpeg"
+    
+}
